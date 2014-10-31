bc3b7e3f08650cb07b29bf4ee85eef459aeb1c5a
Brendan Asselstine <brendan@skyrkt.com>
2014-10-31 15:59:33 -0700

    Finished the footer section.

--- javascripts/swv.js
+++ javascripts/swv.js
@@ -1,1 +1,12 @@
 
+$(document).ready(function() {
+
+  $('ul > li > .head').click(function () {
+    if (!$(this).closest('li').hasClass('active')) {
+        $('li.active').removeClass('active').find("> .content").slideUp();
+        $(this).closest('li').addClass('active').find("> .content").slideDown();
+    }
+  });
+
+  $("ul > li.active > .content").css("display","block");
+});
